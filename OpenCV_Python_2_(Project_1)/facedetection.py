c64336d8cdcc6034d281a1d54361e9d2685771e6
Shafiq-Engineering <shafiq5427.engineering@gmail.com>
2022-12-16 02:23:18 +0800

    update

---
+++ OpenCV_Python_2_(Project_1)/facedetection.py
@@ -0,0 +1,4 @@
+import cv2
+#load pre-trained data
+
+face_trained_data = cv2.CascadeClassifier(r"C:\Users\shafi\Desktop\Data_Science-AI\OpenCV_Python_2_(Project_1)\data\haarcascade_frontalface_alt.xml") #raw string    
